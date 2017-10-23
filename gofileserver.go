00fd6dde7618e9ee2c92035c9c1e326b9dc91cba
Max Zhao <interarticle@gmail.com>
2017-10-23 00:11:20 -0700

    Better logging.

--- gofileserver.go
+++ gofileserver.go
@@ -10,39 +10,48 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"sync/atomic"
+	"time"
 )
 
-var requestCounter uint32
+const responseLogDelay = time.Second
 
 var (
 	rootDirectory = flag.String("root_directory", "", "Path to the folder to be served")
 	bindAddress   = flag.String("bind_address", "", "IP and port to bind the server to")
 )
 
-type ResponseWriterWrapper struct {
-	w http.ResponseWriter
+var requestCounter int64
+
+type responseLogger struct {
+	Writer http.ResponseWriter
+
+	RequestId   int64
+	RemoteAddr  string
+	RequestLine string
+	UserAgent   string
+	Range       string
 
 	Status     int
 	Written    int64
 	WriteError error
+
+	partialLogTimer *time.Timer
 }
 
-func newResponseWriterWrapper(w http.ResponseWriter) *ResponseWriterWrapper {
-	return &ResponseWriterWrapper{
-		w:      w,
-		Status: http.StatusOK,
-	}
+func (w *responseLogger) Init() {
+	w.Status = http.StatusOK
 }
 
-func (w *ResponseWriterWrapper) Header() http.Header {
-	return w.w.Header()
+func (w *responseLogger) Header() http.Header {
+	return w.Writer.Header()
 }
 
-func (w *ResponseWriterWrapper) Write(b []byte) (int, error) {
-	n, err := w.w.Write(b)
+func (w *responseLogger) Write(b []byte) (int, error) {
+	n, err := w.Writer.Write(b)
 	w.Written += int64(n)
 	if err != nil {
 		w.WriteError = err
@@ -50,28 +59,53 @@ func (w *ResponseWriterWrapper) Write(b []byte) (int, error) {
 	return n, err
 }
 
-func (w *ResponseWriterWrapper) WriteHeader(status int) {
+func (w *responseLogger) WriteHeader(status int) {
 	w.Status = status
-	w.w.WriteHeader(status)
+	w.Writer.WriteHeader(status)
 }
 
-type LoggingHandler struct {
+func (w *responseLogger) Log(suffix string) {
+	log.Printf("%s \"%s\" \"%s\" \"%s\" %s", w.RemoteAddr, w.RequestLine, w.UserAgent, w.Range, suffix)
+}
+
+func (w *responseLogger) OnBeforeHandle() {
+	w.partialLogTimer = time.AfterFunc(responseLogDelay,
+		func() {
+			w.Log(fmt.Sprintf("-> 0x%08x", w.RequestId))
+		})
+}
+
+func (w *responseLogger) OnAfterHandle() {
+	suffix := fmt.Sprintf("%d %d \"%v\"", w.Status, w.Written, w.WriteError)
+	if !w.partialLogTimer.Stop() {
+		suffix += fmt.Sprintf(" <- 0x%08x", w.RequestId)
+	}
+	w.Log(suffix)
+}
+
+type loggingHandler struct {
 	Handler http.Handler
 }
 
-func (l LoggingHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	requestId := atomic.AddUint32(&requestCounter, 1)
-	log.Printf("Start %08x %s %s %s %s \"%s\" %s", requestId, r.RemoteAddr, r.Method, r.URL, r.Proto, r.Header.Get("User-Agent"), r.Header.Get("Range"))
-	rww := newResponseWriterWrapper(w)
-	defer func() {
-		log.Printf("Finish %08x %d %d written %v", requestId, rww.Status, rww.Written, rww.WriteError)
-	}()
+func (l loggingHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	rww := &responseLogger{
+		Writer: w,
+
+		RequestId:   atomic.AddInt64(&requestCounter, 1),
+		RemoteAddr:  r.RemoteAddr,
+		RequestLine: fmt.Sprintf("%s %s %s", r.Method, r.URL, r.Proto),
+		UserAgent:   r.Header.Get("User-Agent"),
+		Range:       r.Header.Get("Range"),
+	}
+	rww.Init()
+	rww.OnBeforeHandle()
+	defer rww.OnAfterHandle()
 	l.Handler.ServeHTTP(rww, r)
 }
 
 func main() {
 	flag.Parse()
 	log.Print("Gowebserver started")
-	http.Handle("/", LoggingHandler{http.FileServer(http.Dir(*rootDirectory))})
+	http.Handle("/", loggingHandler{http.FileServer(http.Dir(*rootDirectory))})
 	log.Fatal(http.ListenAndServe(*bindAddress, nil))
 }
