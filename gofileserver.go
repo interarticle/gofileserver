613c34831bf4252ea4e2bd71416727dd466ab62d
Max Zhao <interarticle@gmail.com>
2017-10-23 21:43:49 -0700

    standardized logging.

--- gofileserver.go
+++ gofileserver.go
@@ -1,5 +1,5 @@
 // gofileserver is a simple Go file server that serves a single file system
-// directory on a single port, logging requests to stderr.
+// directory on a single port, logging requests to stdout.
 //
 // gofileserver is intended to serve streaming media reliably on the local
 // network, since the default Go http server implementation does not enforce
@@ -9,15 +9,24 @@
 package main
 
 import (
+	"context"
+	"encoding/csv"
 	"flag"
 	"fmt"
-	"log"
+	"io"
 	"net/http"
+	"os"
+	"os/signal"
+	"sync"
 	"sync/atomic"
+	"syscall"
 	"time"
 )
 
 const responseLogDelay = time.Second
+
+const w3cDate = "2006-01-02"
+const w3cTime = "15:04:05"
 
 var (
 	rootDirectory = flag.String("root_directory", "", "Path to the folder to be served")
@@ -26,24 +35,85 @@ var (
 
 var requestCounter int64
 
+type w3cLogWriter struct {
+	Writer io.Writer
+
+	csvWriter *csv.Writer
+	mutex     sync.Mutex
+}
+
+func newW3cLogWriter(w io.Writer) *w3cLogWriter {
+	wr := &w3cLogWriter{
+		Writer:    w,
+		csvWriter: csv.NewWriter(w),
+	}
+
+	wr.csvWriter.Comma = ' '
+	return wr
+}
+
+func (w *w3cLogWriter) Write(fields []string) {
+	w.mutex.Lock()
+	defer w.mutex.Unlock()
+	w.writeFields(fields)
+}
+func (w *w3cLogWriter) writeFields(fields []string) {
+	err := w.csvWriter.Write(fields)
+	if err != nil {
+		panic(err)
+	}
+	w.csvWriter.Flush()
+	err = w.csvWriter.Error()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (w *w3cLogWriter) WriteCommented(fields []string) {
+	w.mutex.Lock()
+	defer w.mutex.Unlock()
+	_, err := w.Writer.Write([]byte("#"))
+	if err != nil {
+		panic(err)
+	}
+	w.writeFields(fields)
+}
+
+func (w *w3cLogWriter) WriteComment(comment string) {
+	w.mutex.Lock()
+	defer w.mutex.Unlock()
+	_, err := w.Writer.Write([]byte("#" + comment + "\n"))
+	if err != nil {
+		panic(err)
+	}
+}
+
 type responseLogger struct {
 	Writer http.ResponseWriter
 
-	RequestId   int64
-	RemoteAddr  string
-	RequestLine string
-	UserAgent   string
-	Range       string
+	Logger    *w3cLogWriter
+	WaitGroup *sync.WaitGroup
+
+	RequestId  int64
+	RemoteAddr string
+	Method     string
+	URI        string
+	Protocol   string
+	UserAgent  string
+	Range      string
 
 	Status     int
 	Written    int64
 	WriteError error
+
+	Started time.Time
 
 	partialLogTimer *time.Timer
 }
 
 func (w *responseLogger) Init() {
 	w.Status = http.StatusOK
+	w.Started = time.Now()
 }
 
 func (w *responseLogger) Header() http.Header {
@@ -64,38 +134,64 @@ func (w *responseLogger) WriteHeader(status int) {
 	w.Writer.WriteHeader(status)
 }
 
-func (w *responseLogger) Log(suffix string) {
-	log.Printf("%s \"%s\" \"%s\" \"%s\" %s", w.RemoteAddr, w.RequestLine, w.UserAgent, w.Range, suffix)
+func (w *responseLogger) MakePrefixFields() []string {
+	t := time.Now()
+	return []string{
+		t.Format(w3cDate), t.Format(w3cTime), w.RemoteAddr, w.Method, w.URI,
+		w.Protocol, w.UserAgent, w.Range,
+	}
 }
 
 func (w *responseLogger) OnBeforeHandle() {
+	w.WaitGroup.Add(1)
 	w.partialLogTimer = time.AfterFunc(responseLogDelay,
 		func() {
-			w.Log(fmt.Sprintf("-> 0x%08x", w.RequestId))
+			defer w.WaitGroup.Done()
+			w.Logger.WriteCommented(append(w.MakePrefixFields(), "->", fmt.Sprintf("0x%08x", w.RequestId)))
 		})
 }
 
 func (w *responseLogger) OnAfterHandle() {
-	suffix := fmt.Sprintf("%d %d \"%v\"", w.Status, w.Written, w.WriteError)
-	if !w.partialLogTimer.Stop() {
-		suffix += fmt.Sprintf(" <- 0x%08x", w.RequestId)
+	suffix := []string{
+		fmt.Sprintf("%d", w.Status),
+		fmt.Sprintf("%d", w.Written),
+		fmt.Sprintf("%f", time.Now().Sub(w.Started).Seconds()),
 	}
-	w.Log(suffix)
+	if w.WriteError != nil {
+		suffix = append(suffix, w.WriteError.Error())
+	} else {
+		suffix = append(suffix, "")
+	}
+	if !w.partialLogTimer.Stop() {
+		suffix = append(suffix, fmt.Sprintf("<- 0x%08x", w.RequestId))
+	} else {
+		suffix = append(suffix, "")
+		w.WaitGroup.Done()
+	}
+	w.Logger.Write(append(w.MakePrefixFields(), suffix...))
 }
 
 type loggingHandler struct {
-	Handler http.Handler
+	Handler   http.Handler
+	Logger    *w3cLogWriter
+	WaitGroup *sync.WaitGroup
 }
 
 func (l loggingHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	l.WaitGroup.Add(1)
+	defer l.WaitGroup.Done()
 	rww := &responseLogger{
-		Writer: w,
+		Writer:    w,
+		Logger:    l.Logger,
+		WaitGroup: l.WaitGroup,
 
-		RequestId:   atomic.AddInt64(&requestCounter, 1),
-		RemoteAddr:  r.RemoteAddr,
-		RequestLine: fmt.Sprintf("%s %s %s", r.Method, r.URL, r.Proto),
-		UserAgent:   r.Header.Get("User-Agent"),
-		Range:       r.Header.Get("Range"),
+		RequestId:  atomic.AddInt64(&requestCounter, 1),
+		RemoteAddr: r.RemoteAddr,
+		Method:     r.Method,
+		URI:        r.URL.String(),
+		Protocol:   r.Proto,
+		UserAgent:  r.Header.Get("User-Agent"),
+		Range:      r.Header.Get("Range"),
 	}
 	rww.Init()
 	rww.OnBeforeHandle()
@@ -105,7 +201,53 @@ func (l loggingHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	flag.Parse()
-	log.Print("Gowebserver started")
-	http.Handle("/", loggingHandler{http.FileServer(http.Dir(*rootDirectory))})
-	log.Fatal(http.ListenAndServe(*bindAddress, nil))
+	ctx, cancel := context.WithCancel(context.Background())
+
+	logger := newW3cLogWriter(os.Stdout)
+	var wg sync.WaitGroup
+	http.Handle("/", loggingHandler{
+		Handler:   http.FileServer(http.Dir(*rootDirectory)),
+		Logger:    logger,
+		WaitGroup: &wg,
+	})
+
+	server := &http.Server{
+		Addr: *bindAddress,
+	}
+
+	logger.WriteComment("Version: 1.0")
+	logger.WriteComment(fmt.Sprintf("Date: %s %s", time.Now().Format(w3cDate), time.Now().Format(w3cTime)))
+	logger.WriteComment("Fields: date time c-ip cs-method cs-uri x-cs-protocol cs(User-Agent) cs(Range) sc-status bytes time-taken x-write-error x-async-association")
+
+	wg.Add(1)
+	go func() {
+		defer cancel()
+		defer wg.Done()
+		err := server.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			logger.WriteComment("Error: " + err.Error())
+		}
+	}()
+
+	go func() {
+		cancelCtx, _ := context.WithTimeout(ctx, time.Second)
+		<-cancelCtx.Done()
+		if cancelCtx.Err() == context.DeadlineExceeded {
+			logger.WriteComment("Status: gofileserver started")
+		}
+	}()
+
+	go func() {
+		defer cancel()
+		sigC := make(chan os.Signal, 2)
+		signal.Notify(sigC, os.Interrupt, syscall.SIGTERM)
+
+		<-sigC
+	}()
+
+	<-ctx.Done()
+	logger.WriteComment("Status: Shutting down")
+	server.Close()
+	wg.Wait()
+	logger.WriteComment("Status: gofileserver shutdown")
 }
